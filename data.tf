71fda06e5f83f644c3705a1bd9e1c9c33dac97fd
ramvelcheti <ramwalfox@gmail.com>
2025-12-26 18:37:36 +0530

    terraform

--- data.tf
+++ data.tf
@@ -23,7 +23,7 @@ data "aws_ssm_parameter" "sg_id" {
 }
 
 data "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project_name}/${var.environment}/${var.component}/vpc_id"
+  name = "/${var.project_name}/${var.environment}/vpc_id"
 }
 
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
